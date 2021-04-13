2eb0438bbccde098ea5e9123ab5a5f5c9fba0dcb
Jordi Llobet <jordi.llobet@uab.cat>
2021-04-13 13:02:16 +0200

    Fix color numbers and bloc wheel zoom

--- NetworkViewer/js/NetworkViewer.js
+++ NetworkViewer/js/NetworkViewer.js
@@ -1,83 +1,78 @@
 require([
-      "esri/Map",
-      "esri/views/MapView",
-      "esri/layers/FeatureLayer",
-      "esri/widgets/Zoom"
-],
-      function (Map, MapView, FeatureLayer, Zoom) {
+        "esri/Map",
+        "esri/views/MapView",
+        "esri/layers/FeatureLayer",
+        "esri/widgets/Zoom"
+    ],
+    function(Map, MapView, FeatureLayer, Zoom) {
 
-            //configuració portal
-            //esriConfig.portalUrl = "https://uab.maps.arcgis.com";
-            //esriConfig.request.proxyUrl = "proxy.jsp";
+        //configuració portal
+        //esriConfig.portalUrl = "https://uab.maps.arcgis.com";
+        //esriConfig.request.proxyUrl = "proxy.jsp";
 
 
-            const sym = {
-                  type: "simple",  // autocasts as new SimpleRenderer()
-                  symbol: {
-                        type: "simple-marker",  // autocasts as new SimpleMarkerSymbol()
-                        style: "circle",
-                        size: 18,
-                        color: "#B0DBBF",
-                        outline: {  // autocasts as new SimpleLineSymbol()
-                              width: 0.5,
-                              color: "black"
-                        }
-                  },
-                  popupTemplate: {
-                        title: "Hola",
-                        /*content: "{state_name}"*/
-                  }
-            };
+        const renderer = {
+            type: "simple", // autocasts as new SimpleRenderer()
+            symbol: {
+                type: "simple-marker", // autocasts as new SimpleMarkerSymbol()
+                style: "circle",
+                size: 18,
+                color: "#B0DBBF",
+                outline: { // autocasts as new SimpleLineSymbol()
+                    width: 0.5,
+                    color: "#9abf9e"
+                }
+            },
+            visualVariables: [{
+                type: "size",
+                field: "num",
+                stops: [{
+                        value: 5,
+                        size: 10
+                    },
+                    {
+                        value: 15,
+                        size: 20
+                    },
+                    {
+                        value: 30,
+                        size: 30
+                    }
+                ]
+            }]
+        };
 
-            const defaultPosition = [2.13, 41.40];
+        const defaultPosition = [2.13, 41.40];
 
-            var popupInfo = {
-                  title: "{country}",
-                  content:
-                        "Institut: {institutio}"
-            };
+        var popupInfo = {
+            title: "{country}",
+            content: "Institut: {institutio}"
+        };
 
-            var layer = new FeatureLayer({
-                  url: "https://openlab.uab.cat:6443/arcgis/rest/services/colaboradors_let/FeatureServer/0",
-                  renderer: sym,
-                  outFields: ["*"],
-                  popupTemplate: popupInfo,
-                  visualVariables: [
-                        {
-                              type: "size",
-                              field: "num",
-                              normalizationField: "num",
-                              stops: [
-                                    {
-                                          value: 5,
-                                          size: 12
-                                    },
-                                    {
-                                          value: 15,
-                                          size: 15
-                                    },
-                                    {
-                                          value: 30,
-                                          size: 18
-                                    }
-                              ]
-                        }
-                  ]
-            });
+        var layer = new FeatureLayer({
+            url: "https://openlab.uab.cat:6443/arcgis/rest/services/webmaps/network_let/FeatureServer/0",
+            renderer: renderer,
+            outFields: ["*"],
+            popupTemplate: popupInfo
+        });
 
-            const map = new Map({
-                  basemap: "topo",
-                  layers: [layer]
-            });
+        const map = new Map({
+            basemap: "topo",
+            layers: [layer]
+        });
 
-            const view = new MapView({
-                  container: "viewDiv",
-                  map: map,
-                  center: defaultPosition,
-                  zoom: 3,
-                  constraints: {
-                        minZoom: 3,
-                        maxZoom: 7
-                  }
-            });
-      });
+        const view = new MapView({
+            container: "viewDiv",
+            map: map,
+            center: defaultPosition,
+            zoom: 3,
+            constraints: {
+                minZoom: 3,
+                maxZoom: 7
+            }
+        });
+
+        view.surface.addEventListener("wheel", function(event) {
+            event.stopImmediatePropagation();
+        }, true);
+    });
