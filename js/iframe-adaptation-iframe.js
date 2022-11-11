11b4b0596593224f80b1639b95660856816329a8
metathairs <daniel.martinez.bou@uab.cat>
2022-11-11 14:00:28 +0100

    simplify js

--- js/iframe-adaptation-iframe.js
+++ js/iframe-adaptation-iframe.js
@@ -1,20 +1,8 @@
-// iframe should have in <body> data-url_parent = "parent_url"
-
 window.addEventListener("load", function() {
-	
-	console.log('load');	
-	console.log('iframe...');
 	
 	var data_url_parent = 'https://iermb.uab.cat/';
 	
-	const body = document.body;
-	const html = document.documentElement;
-	console.log(body)
-	console.log(html)
-	
-	var height = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight ) + 'px';
-	
-	console.log(height)
+	var height = Math.max( document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight ) + 'px';
 	
 	top.postMessage(height, data_url_parent);	
 });
