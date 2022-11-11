62a79b08f51a3d63b80f79575c04564db077949f
metathairs <daniel.martinez.bou@uab.cat>
2022-11-11 11:45:32 +0100

    add js + indicadors frame test

---
+++ js/iframe-adaptation.js
@@ -0,0 +1,40 @@
+// iframe should have in <body> data-url_parent = "parent_url"
+// parent should have in <iframe> class = "iframe_full_height"
+
+window.addEventListener("load", function() {
+
+	var iframes = window.getElementsByClassName('iframe_full_height');
+	
+	Array.prototype.filter.call(iframes, function(iframe) {
+		
+		window.iframe = iframe;
+		
+		window.addEventListener('message',function(event){
+						
+			var iframe = event.currentTarget.iframe;
+					
+			if (iframe.src === undefined ) {
+				return;
+			}
+		
+			if( event.origin !== iframe.src) {
+				return;
+			}
+			
+			iframe.style.height = event.data;			
+			
+		},false);
+	});
+	
+	
+	var data_url_parent = document.body.dataset.url_parent;
+	
+	if (data_url_parent === undefined ) {
+		return;
+	}
+	
+	var height = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight ) + 'px';
+	
+	top.postMessage('page_height', height, data_url_parent);	
+});
+
