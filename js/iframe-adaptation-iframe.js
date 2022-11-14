c4bc4da2c5f8fe18266360c93636757cc56e159a
metathairs <daniel.martinez.bou@uab.cat>
2022-11-14 08:43:04 +0100

    fix js

--- js/iframe-adaptation-iframe.js
+++ js/iframe-adaptation-iframe.js
@@ -1,9 +1,14 @@
 window.addEventListener("load", function() {
 	
+	console.log('iframe addEventListener load');
+	
 	var data_url_parent = 'https://iermb.uab.cat/';
 	
 	var height = Math.max( document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight ) + 'px';
 	
-	top.postMessage(height, data_url_parent);	
+	console.log('height');
+	console.log(height);
+	
+	window.top.postMessage(height, data_url_parent);	
 });
 
